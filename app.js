7d557d1150c9da311c912482c5b523a44a1ad0cc
Paul Beffa <paulbeffa@outlook.fr>
2019-04-02 19:41:36 +0200

    Set up completed, database connection, body parser, templating engine. Ready to begin creating mongodb shema

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 // Setup
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/node-blog")
+
+// body parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// templating engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Routes
+app.get("/", (req, res) => {
+   res.render('index.ejs');
+});
 
 // Listen
 app.listen(3000, () => {
     console.log('Server listing on 3000');
 })
-
