81dbae7005710ee93c64f0fbedf7cfccaecc5754
Paul Beffa <paulbeffa@outlook.fr>
2019-04-03 19:07:07 +0200

    tuto completed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 
 // Database connection
-mongoose.connect("mongodb://localhost:27017/node-blog")
+mongoose.connect("mongodb://localhost:27017/pauloblog")
+
 
 // body parser middleware
 app.use(bodyParser.json())
@@ -15,9 +16,26 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+
+// Mongoose Schema
+var postSchema = new mongoose.Schema({ body: String });
+var Post = mongoose.model('Post', postSchema);
+
+
 // Routes
 app.get("/", (req, res) => {
-   res.render('index.ejs');
+    Post.find({}, (err, posts) => {
+        res.render('index.ejs', { posts: posts})
+    })
+});
+// add post route for saving the request body and redirecting to the root route
+app.post('/addpost', (req, res) => {
+    var postData = new Post(req.body);
+    postData.save().then( result => {
+        res.redirect('/');
+    }).catch(err => {
+        res.status(400).send("Unable to save data");
+    });
 });
 
 // Listen
